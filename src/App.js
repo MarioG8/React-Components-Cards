15385c804e5b3d1c5efa0673bbdcda66f15dce43
MarioG8 <golikmariusz7@gmail.com>
2021-04-11 13:04:20 +0200

    Added new card complete component based on grid

--- src/App.js
+++ src/App.js
@@ -1,9 +1,60 @@
 import "./App.scss";
+import Card from "./components/Card";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Mariusz Golik Full-stack JavaScript Developer.Hello,World!</h1>
+    <div className="app">
+      <h1>Mountains</h1>
+      <div className="wrapper">
+        <Card
+          img="https://images.unsplash.com/photo-1454496522488-7a8e488e8606?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1355&q=80"
+          title="Mountains - Ecosse"
+          description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga aperiam
+        "
+        />
+        <Card
+          img="https://images.unsplash.com/photo-1434394354979-a235cd36269d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1333&q=80"
+          title="Mountains - Marmolada Italy"
+          description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga aperiam
+        "
+        />
+        <Card
+          img="https://images.unsplash.com/photo-1469362102473-8622cfb973cd?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1393&q=80"
+          title="Mountains - Polish Tatra"
+          description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga aperiam
+        "
+        />
+        <Card
+          img="https://images.unsplash.com/photo-1463003416389-296a1ad37ca0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80"
+          title="Mountains - Ecosse"
+          description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga aperiam
+        "
+        />
+        <Card
+          img="https://images.unsplash.com/photo-1534419234121-27689a5fcd71?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+          title="Mountains - Marmolada Italy"
+          description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga aperiam
+        "
+        />
+        <Card
+          img="https://images.unsplash.com/photo-1530295314625-30d3b777ac7a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80"
+          title="Mountains - Polish Tatra"
+          description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga aperiam
+        "
+        />
+        <Card
+          img="https://images.unsplash.com/photo-1534419234121-27689a5fcd71?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+          title="Mountains - Marmolada Italy"
+          description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga aperiam
+        "
+        />
+        <Card
+          img="https://images.unsplash.com/photo-1530295314625-30d3b777ac7a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80"
+          title="Mountains - Polish Tatra"
+          description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga aperiam
+        "
+        />
+      </div>
     </div>
   );
 }
